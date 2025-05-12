44380a645a9002cb69e8dfc73802df18be3b1d86
ianoruga <orugaian@gmail.com>
2025-05-12 14:17:24 +0800

    Fix path to .glb file and update Vite base config

--- src/Robotics_2.jsx
+++ src/Robotics_2.jsx
@@ -7,7 +7,7 @@ import React from "react";
 import { useGLTF } from "@react-three/drei";
 
 export default function Model(props) {
-  const { nodes, materials } = useGLTF("/Robotics_2.glb");
+  const { nodes, materials } = useGLTF("/viewing2/Robotics_2.glb");
   return (
     <group {...props} dispose={null}>
       <mesh
